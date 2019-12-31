3c6b237f07ad8a4413854fdf47228406df5fb678
Alex Parra <alexpds@gmail.com>
2019-12-31 11:12:32 +0000

    Improved secondsToHms

--- src/utils/secondsToHms.js
+++ src/utils/secondsToHms.js
@@ -5,9 +5,7 @@ const secondsToHms = seconds => {
   const days = Math.floor( seconds / SECONDS_PER_DAY );
   const remainderSeconds = seconds % SECONDS_PER_DAY;
   const hms = new Date(remainderSeconds * 1000).toISOString().substring(11, 19);
-  const hhmmss = hms.split(':');
-  hhmmss[0] = `${Number(hhmmss[0]) + (days * HOURS_PER_DAY)}`.padStart(2, '0');
-  return hhmmss.join(':');
+  return hms.replace(/^(\d+)/, h => `${Number(h) + days * HOURS_PER_DAY}`.padStart(2, '0'));
 };
 
 export default secondsToHms;
